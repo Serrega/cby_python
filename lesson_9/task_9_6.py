a3f97488f3cfaaddd0606f6398880d70e9af8b25
Serj Boris <priserv@tuta.io>
2022-05-20 14:16:04 +0300

    task_9_6

--- lesson_9/task_9_6.py
+++ lesson_9/task_9_6.py
@@ -2,39 +2,49 @@ import pyglet
 from random import randint
 
 
-vidPath = 'Need_for_Speed.mp4'
-window = pyglet.window.Window(1280, 720)
-player = pyglet.media.Player()
-source = pyglet.media.StreamingSource()
-MediaLoad = pyglet.media.load(vidPath)
+def video_play():
+    vidPath = 'Need_for_Speed.mp4'
+    window = pyglet.window.Window(1280, 720)
+    player = pyglet.media.Player()
+    source = pyglet.media.StreamingSource()
+    MediaLoad = pyglet.media.load(vidPath)
 
-player.queue(MediaLoad)
-player.play()
+    player.queue(MediaLoad)
+    player.play()
+
+    @window.event
+    def on_draw():
+        window.clear()
+        if player.source and player.source.video_format:
+            player.get_texture().blit(0, 0)
+
+    pyglet.app.run()
 
 
-@window.event
-def on_draw():
-    window.clear()
-    if player.source and player.source.video_format:
-        player.get_texture().blit(0, 0)
-
-
-pyglet.app.run()
-
-try:
-    with open('result.txt', 'r') as file_r:
-        content = file_r.readlines()
-except BaseException as be:
-    print('file error - ', be.args)
+def paint_pictures(win: bool):
+    try:
+        with open('result.txt', 'r', encoding='utf-8') as file_r:
+            content = file_r.readlines()
+    except BaseException as be:
+        print('file error - ', be.args)
+    if win:
+        print('\n', *content[20:])
+    else:
+        print(*content[:20])
 
 
 if __name__ == '__main__':
     road = (150, 100)
     user_speed = randint(150, 250)
-    user_time = (road[0]/user_speed, road[1]/user_speed)
-    win_number = user_time.index(max(user_time))
-    print(win_number)
-    user_input = int(input('Enter number of road, 1 - track, 2 - dirt road: '))
+    user_time = (road[0] / user_speed, road[1] / user_speed)
+    win_number = user_time.index(min(user_time))
+    video_play()
+    user_input = int(
+        input('Enter number of road, 1 - track, 2 - dirt road: '))
+    if user_input - 1 == win_number:
+        paint_pictures(True)
+    else:
+        paint_pictures(False)
 
 
 
